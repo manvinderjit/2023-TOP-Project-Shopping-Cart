d62c2c0b35d405cab80a8fa5c06f3cbb3b2f2f41
Manvinderjit <91133409+manvinderjit@users.noreply.github.com>
2023-11-02 01:58:29 -0400

    feat: Add cart functions
    Add cart empty, remove, increment, and decrement item features.
    Add horizontal list layout for cart.

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,58 +1,164 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { Button, Container, Card, ListGroup } from 'react-bootstrap';
-import { removeFromCart } from '../features/cart/cartSlice';
+import { Button, Container, Toast, ToastContainer, Row, Col, Image } from 'react-bootstrap';
+import { removeFromCart, removeAllFromCart, decrementItemQuantity, addToCart } from '../features/cart/cartSlice';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Cart = () => {
+    const cart = useSelector((state) => state.cart);
     const cartItems = useSelector((state) => state.cart.cartItems);
     const dispatch = useDispatch();
+    const [showMessageToast, setShowMessageToast] = useState(false);
+    const [textContentOfToast, setTextContentOfToast] = useState('');
+
+    const emptyCart = async () => {
+        await dispatch(removeAllFromCart());
+        
+        checkIfCartIsEmpty()
+            ? setTextContentOfToast('Cart emptied')
+            : setTextContentOfToast('Error! Could not empty cart. Try again');
+        setShowMessageToast(true);
+    }
+
+    const checkIfCartIsEmpty = () => {
+        return cartItems.length === 0
+            ? true
+            : false;
+    }
+
+    const handleDecrementItemQuantity = (id) => {
+        dispatch(decrementItemQuantity(id));
+    }
+
+    const handleIncrementItemQuantity = (item) => {
+        dispatch(addToCart(item));
+    };
+
+    const removeItemFromCart = (event) => {
+        dispatch(removeFromCart(event.target.value));
+    }
+    
     return (
         <>
             <Container className="d-flex flex-column">
-                <h2>Cart Items</h2>
+                <h2>Your Cart Items</h2>
 
-                <Container className="d-flex justify-content-evenly">
-                    {cartItems.map((item) => {
-                        return (
-                            <>
-                                <Card
-                                    style={{ width: '18rem' }}
-                                    className="p-0 mb-4"
-                                >
-                                    <Card.Img
-                                        variant="top"
-                                        src="holder.js/100px180?text=Image cap"
-                                    />
-                                    <Card.Body>
-                                        <Card.Title>{item.name}</Card.Title>
-                                        <hr />
-                                        <Card.Text style={{ height: '4rem' }}>
+                <ToastContainer
+                    style={{ zIndex: 1, marginTop: '5rem' }}
+                    position="top-center"
+                >
+                    <Toast
+                        onClose={() => setShowMessageToast(false)}
+                        show={showMessageToast}
+                        delay={3000}
+                        autohide
+                    >
+                        <Toast.Body className="bg-success text-light">
+                            <strong className="me-auto fs-6">
+                                {textContentOfToast}
+                            </strong>
+                        </Toast.Body>
+                    </Toast>
+                </ToastContainer>
+                {cartItems.length === 0 ? (
+                    <h5>Your cart is currently empty!</h5>
+                ) : (
+                    <Container className="d-flex my-4 flex-column justify-content-evenly">
+                        <Row>
+                            <Col className="h6 border m-0 p-2">Image</Col>
+                            <Col className="h6 border m-0 p-2">Name</Col>
+                            <Col className="h6 border m-0 p-2 col-3">
+                                Description
+                            </Col>
+                            <Col className="h6 border m-0 p-2">Unit Price</Col>
+                            <Col className="h6 border m-0 p-2 col-1">
+                                Quantity
+                            </Col>
+                            <Col className="h6 border m-0 p-2">Total Price</Col>
+                            <Col className="h6 border m-0 p-2">Remove</Col>
+                        </Row>
+                        {cartItems.map((item) => {
+                            return (
+                                <>
+                                    <Row>
+                                        <Col className="h6 border m-0 p-2 d-flex justify-content-center align-items-center d-flex">
+                                            <Image src="holder.js/100px180?text=Image cap"></Image>
+                                        </Col>
+                                        <Col className="border m-0 p-2 d-flex justify-content-center align-items-center">
+                                            {item.name}
+                                        </Col>
+                                        <Col className="border m-0 p-2 d-flex justify-content-center align-items-center col-3">
                                             {item.description}
-                                        </Card.Text>
-                                    </Card.Body>
-                                    <ListGroup className="list-group-flush">
-                                        <ListGroup.Item>
-                                            Product Price: ${item.price}
-                                        </ListGroup.Item>
-                                        <ListGroup.Item>
-                                            Stock: {item.stock}
-                                        </ListGroup.Item>
-                                    </ListGroup>
-                                    <Card.Body>
+                                        </Col>
+                                        <Col className="border m-0 p-2 d-flex justify-content-center align-items-center">
+                                            {item.price}
+                                        </Col>
+                                        <Col className="border m-0 p-2 d-flex justify-content-evenly align-items-center col-1">
+                                            <Button className="btn-sm btn-light" onClick={() => { handleDecrementItemQuantity(item._id) }}>
+                                                -
+                                            </Button>
+                                            {item.itemQuantity}
+                                            <Button className="btn-sm btn-light" onClick={() => handleIncrementItemQuantity(item)}>
+                                                +
+                                            </Button >
+                                        </Col>
+                                        <Col className="border m-0 p-2 d-flex justify-content-center align-items-center">
+                                            {item.price * item.itemQuantity}
+                                        </Col>
+                                        <Col className="border m-0 p-2 d-flex justify-content-center align-items-center">
+                                            <Button
+                                                className="btn btn-sm"
+                                                onClick={removeItemFromCart}
+                                                value={item._id}
+                                            >
+                                                Remove
+                                            </Button>
+                                        </Col>
+                                    </Row>
+                                </>
+                            );
+                        })}
+                    </Container>
+                )}
+                <Container className="d-flex flex-column gap-3">
+                    {cartItems.length > 0 ? (
+                        <>
+                            <Row className="d-flex justify-content-end">
+                                SubTotal: ${cart.totalAmount}
+                            </Row>
+                            <Row>
+                                <Col className="d-flex justify-content-start">
+                                    <Button
+                                        className="mx-2 "
+                                        onClick={emptyCart}
+                                    >
+                                        Empty Cart
+                                    </Button>
+                                </Col>
+                                <Col className="d-flex justify-content-end">
+                                    <Row>
                                         <Button
-                                            className="mx-2"
-                                            onClick={() => {
-                                                dispatch(
-                                                    removeFromCart(item._id),
-                                                );
-                                            }}
+                                            className="mx-2 "
+                                            onClick={emptyCart}
                                         >
-                                            Remove from Cart
+                                            Checkout
                                         </Button>
-                                    </Card.Body>
-                                </Card>
-                            </>
-                        );
-                    })}
+                                    </Row>
+                                </Col>
+                            </Row>
+
+                            <Row className="d-flex justify-content-end">
+                                <Link
+                                    className="d-flex justify-content-end"
+                                    to={'/'}
+                                >
+                                    Continue Shopping
+                                </Link>
+                            </Row>
+                        </>
+                    ) : (
+                        <></>
+                    )}
                 </Container>
             </Container>
         </>
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -3,23 +3,61 @@ import { createSlice } from '@reduxjs/toolkit';
 export const cartSlice = createSlice({
     name: 'cart',
     initialState: {
-        cartItems: [],
+        cartItems: localStorage.getItem('cartItems')
+            ? JSON.parse(localStorage.getItem('cartItems'))
+            : [],
         status: null,
-        totalPrice: 0,
+        totalAmount: 0,
     },
     reducers: {
         addToCart: (state, action) => {
-            state.cartItems.push(action.payload);
+            // Check if the cart already contains the item, find its index
+            const itemIndex = state.cartItems.findIndex(
+                (item) => item._id === action.payload._id,
+            );
+
+            if (itemIndex >= 0) {
+                state.cartItems[itemIndex].itemQuantity += 1;
+            } else {
+                const tempCartItem = { ...action.payload, itemQuantity: 1 };
+                state.cartItems.push(tempCartItem);
+            }
+            localStorage.setItem('cartItems', JSON.stringify(state.cartItems));
         },
+
         removeFromCart: (state, action) => {
-            state.cartItems = state.cartItems.filter(
+            const newCartItems = state.cartItems.filter(
                 (item) => item._id !== action.payload,
             );
+            state.cartItems = newCartItems;
+            localStorage.setItem('cartItems', JSON.stringify(state.cartItems));
         },
+
+        decrementItemQuantity: (state, action) => {
+            const itemIndex = state.cartItems.findIndex(
+                (item) => item._id === action.payload,
+            );
+                
+            if(state.cartItems[itemIndex].itemQuantity > 1){
+                state.cartItems[itemIndex].itemQuantity -= 1;
+            } else if (state.cartItems[itemIndex].itemQuantity == 1 ) {
+                const newCartItems = state.cartItems.filter(
+                    (item) => item._id !== action.payload,
+                );
+                state.cartItems = newCartItems;
+            }
+            localStorage.setItem('cartItems', JSON.stringify(state.cartItems));
+        },
+
+        removeAllFromCart: (state) => {
+            localStorage.removeItem('cartItems');
+            state.cartItems = [];
+            state.totalAmount = 0;
+        }
     },
 });
 
 // Action creators are generated for each case reducer function
-export const { addToCart, removeFromCart } = cartSlice.actions;
+export const { addToCart, removeFromCart, decrementItemQuantity, removeAllFromCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
