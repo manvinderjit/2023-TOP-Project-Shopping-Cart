325d3f21e8886a580debd2c83f3c93d49bd59c3f
Manvinderjit <91133409+manvinderjit@users.noreply.github.com>
2024-08-09 17:24:22 -0400

    fix: Highlight current page in nav

--- src/components/nav/NavItemsTop.tsx
+++ src/components/nav/NavItemsTop.tsx
@@ -1,31 +1,32 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useAppSelector } from "../../application/reduxHooks";
 import { getCurrentToken, getCurrentUserDetails } from "../../features/auth/authSlice";
 
 
 const NavItemsTop = (): React.JSX.Element => {
   const token = useAppSelector(getCurrentToken);
+  const currentLocation = useLocation();
 
     const content: React.JSX.Element = (
       <ul className="flex flex-row mr-auto">
-        <li className="flex flex-col justify-center text-center w-24 text-l">
+        <li className={`flex flex-col justify-center text-center w-24 text-l ${currentLocation.pathname === '/' ? 'text-[#646cff]' : ''}`}>
           <Link to="/">Home</Link>
         </li>
         {token && token !== null ? (
-          <li className="flex flex-col justify-center text-center w-24 text-l">
+          <li className={`flex flex-col justify-center text-center w-24 text-l ${currentLocation.pathname === '/dashboard' ? 'text-[#646cff]' : ''}`}>
             <Link to="/dashboard">Dashboard</Link>
           </li>
         ) : (
           <>
-            <li className="flex flex-col justify-center text-center w-24 text-l">
+            <li className={`flex flex-col justify-center text-center w-24 text-l ${currentLocation.pathname === '/login' ? 'text-[#646cff]' : ''}`}>
               <Link to="/login">Login</Link>
             </li>
-            <li className="flex flex-col justify-center text-center w-24 text-l">
+            <li className={`flex flex-col justify-center text-center w-24 text-l ${currentLocation.pathname === '/register' ? 'text-[#646cff]' : ''}`}>
               <Link to="/register">Register</Link>
             </li>
           </>
         )}
-        <li className="flex flex-col justify-center text-center w-24 text-l">
+        <li className={`flex flex-col justify-center text-center w-24 text-l ${currentLocation.pathname === '/cart' ? 'text-[#646cff]' : ''}`}>
           <Link to="/cart">Cart</Link>
         </li>
       </ul>
