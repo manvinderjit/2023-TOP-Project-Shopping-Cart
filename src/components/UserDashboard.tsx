b0972ceeccc059281bfcb2c5a3de4beebbe656b4
Manvinderjit <91133409+manvinderjit@users.noreply.github.com>
2024-08-21 18:50:23 -0400

    fix: Remove redundant Logout button from user dashboard

--- src/components/UserDashboard.tsx
+++ src/components/UserDashboard.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { useAppDispatch, useAppSelector } from "../application/reduxHooks";
+import { useAppSelector } from "../application/reduxHooks";
 import { getCurrentToken, getCurrentUserDetails, logOut } from "../features/auth/authSlice";
 import { useNavigate } from "react-router-dom";
 import { useGetUserOrdersQuery } from "../features/api/apiSlice";
@@ -7,7 +7,6 @@ import { useGetUserOrdersQuery } from "../features/api/apiSlice";
 const UserDashboard = () => {
     const username = useAppSelector(getCurrentUserDetails);
     const token = useAppSelector(getCurrentToken);
-    const dispatch = useAppDispatch();
     const navigate = useNavigate();
 
     const {
@@ -18,31 +17,19 @@ const UserDashboard = () => {
       error: errorUserOrders,
     } = useGetUserOrdersQuery(token);
 
-    // const orders = dispatch(useGetUserOrdersQuery(token));
-
-    const handleLogout = () => {
-        dispatch(logOut());
-    }
-
     useEffect(() => {
         if(!token || token === null) navigate('/login');
     },[navigate, token])
 
-    useEffect(() => {
-      if (isSuccessUserOrders) console.log(userOrders);
-    }, [isSuccessUserOrders, userOrders]);
+    // TODO: Display user Orders
+    // useEffect(() => {
+    //   if (isSuccessUserOrders) console.log(userOrders);
+    // }, [isSuccessUserOrders, userOrders]);
 
     const content = (
       <div className="w-full h-full flex flex-col justify-center items-center">
         <h2>Dashboard</h2>
         <p>Welcome {username}</p>
-        <button
-          type="button"
-          onClick={handleLogout}
-          className="flex w-48 justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-        >
-          Logout
-        </button>
       </div>
     );
     
